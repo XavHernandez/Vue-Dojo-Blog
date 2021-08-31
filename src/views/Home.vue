ba8410e84279a12bda4f7dc0dc29a64dc6ae7124
Xavier Hernandez <xhernan.martinez@gmail.com>
2021-08-31 16:07:44 +0200

    Reactive Template refs

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,22 @@
 <template>
   <div class="home">
-    
+    <h1>Home</h1>
   </div>
 </template>
 
 <script>
+import { ref } from '@vue/reactivity'
+
 export default {
   name: 'Home',
-  components: {}
+  components: {},
+  setup() {
+    const posts = ref([
+      { title: 'Welcome to the Blog', body: 'Lorem Ipsum', id: 1 },
+      { title: 'Top 5 CSS Tips', body: 'Lorem Ipsum', id: 2 }
+    ])
+
+    return { posts }
+  }
 }
 </script>

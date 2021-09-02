a61b6f102552ce6bd847b162ceb9d4b045ac50ad
Xavier Hernandez <xhernan.martinez@gmail.com>
2021-09-02 18:09:50 +0200

    Async data / Composition API / Lifecycle Hooks

---
+++ src/components/PostList.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="post-list" v-for="post in posts" :key="post.id">
+    <SinglePost :post="post" />
+  </div>
+</template>
+
+<script>
+import SinglePost from './SinglePost'
+
+export default {
+  props: ['posts'],
+  components: {
+    SinglePost
+  }
+}
+</script>
+
+<style>
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,22 +1,56 @@
 <template>
   <div class="home">
     <h1>Home</h1>
+    <div class="error" v-if="error">{{ error }}</div>
+    <div v-if="posts.length">
+      <PostList  v-if="showPost" :posts="posts"/>
+    </div>
+    <div v-else>Loading...</div>
+    <button @click="showPost=!showPost">Toggle Posts</button>
   </div>
 </template>
 
 <script>
+import PostList from '@/components/PostList.vue'
 import { ref } from '@vue/reactivity'
 
 export default {
   name: 'Home',
-  components: {},
+  components: {
+    PostList
+  },
   setup() {
-    const posts = ref([
-      { title: 'Welcome to the Blog', body: 'Lorem Ipsum', id: 1 },
-      { title: 'Top 5 CSS Tips', body: 'Lorem Ipsum', id: 2 }
-    ])
+    const posts = ref([])
+    const error = ref(null)
 
-    return { posts }
+    const showPost = ref(true)
+
+    const load = async () => {
+      try { 
+        let data = await fetch('http://localhost:3000/posts')
+        if (!data.ok) {
+          throw Error ('No data available')
+        }
+        posts.value = await data.json()
+      } 
+      catch (err) {
+        error.value = err.message
+      }
+    }
+
+    load()
+
+    return { posts, showPost, error }
   }
 }
 </script>
+
+<style>
+button {
+  border: 0;
+  padding: 10px;
+  background-color: crimson;
+  color: white;
+  border-radius: 5px;
+}
+</style>
